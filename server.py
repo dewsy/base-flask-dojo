7aaef69f1e3ae2a60de0cfd76e6cd8ee5768b54e
helgahorvath <h.horvath302@gmail.com>
2018-05-24 09:44:21 +0200

    something

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
-import data_manager
+import utility
 from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
+@app.route('/applicant')
+def first_names_list():
+    first_names = utility.get_first_names()
+    return render_template('index.html', first_names=first_names)
+
+
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True
+        host = '0.0.0.0',
+        debug = True,
+        port = 5000
+    )
---
+++ utility.py
@@ -0,0 +1,10 @@
+import database_common
+
+
+@database_common.connection_handler
+def get_first_names(cursor):
+    cursor.execute("""
+                    SELECT first_name from applicants
+                   """)
+    first_names = cursor.fetchall()
+    return first_names

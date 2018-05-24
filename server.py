2d4ca8f2ec2ca88f95e1ba18946a44a0d3422204
Dewsy <dewsyy@gmail.com>
2018-05-24 09:57:04 +0200

    flask variables set, display now readable

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route('/')
 @app.route('/applicant')
 def first_names_list():
     first_names = utility.get_first_names()
@@ -15,8 +17,7 @@ def first_names_list():
 
 if __name__ == '__main__':
     app.run(
-        debug=True
+        debug=True,
         host = '0.0.0.0',
-        debug = True,
         port = 5000
     )
